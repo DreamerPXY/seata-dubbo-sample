bcfc0078ff82607598e322598bd8b7e1c0676a99
zero.peng <zero.peng@jtexpress.com>
2023-03-17 17:57:14 +0800

    feat:first commit2

--- dubbo-provider/src/main/java/com/example/service/MainApplication.java
+++ dubbo-provider/src/main/java/com/example/service/MainApplication.java
@@ -1,9 +1,11 @@
 package com.example.service;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class,args);
